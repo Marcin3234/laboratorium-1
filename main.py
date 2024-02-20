e335bf51644c93a69b2e31a290c0a22daa5913b5
Marcin3234 <marcin.salek0@wp.pl>
2024-02-20 16:40:17 +0100

    lab1

---
+++ main.py
@@ -0,0 +1,24 @@
+print("ZADANIE 1")
+#range
+for h in range(10):
+    print(h)
+#help
+def p(r):
+    '''
+    Funkcja pokazująca helpa
+    '''
+
+help(p)
+#chr zamienia na ASCII
+print("Funkcja chr",chr(89))
+#hash zwraca wartosc hash
+print("Funkcja hash",hash(p))
+#abs liczba bezwgledna
+print("Funkcja abs",abs(h))
+
+print("ZADANIE 2")
+def a(b):
+    print("wywołana funkcja a",b)
+print(a(2))
+
+print("ZADANIE 3")
