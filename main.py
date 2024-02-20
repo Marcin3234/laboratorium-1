ea57d51bee6dd244a46aa4bf198869aa11a6e2ac
Marcin3234 <marcin.salek0@wp.pl>
2024-02-20 16:59:28 +0100

    lab1

--- main.py
+++ main.py
@@ -18,8 +18,23 @@ print("Funkcja abs",abs(h))
 
 print("ZADANIE 2")
 def a(b):
+    print("wywołana funkcja a to:",b)
+print(a(5))
+
+print("ZADANIE 3")
+def a(b):
     print("wywołana funkcja a",b)
 print(a(2))
 
-print("ZADANIE 3")
+print("ZADANIE 4")
+def fun1(text):
+    print(text)
+def fun2(super, arg="super"):
+    print("jesteś")
+    super(arg)
+    print("duży")
+print("START")
+fun2(fun1, "Tekst do funkcji wewnetrznej")
+print("KONIEC")
 
+print("ZADANIE 5")
