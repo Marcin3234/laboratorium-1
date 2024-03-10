d00edb2f9f0ac5e5d56120af7b144253fb4456ca
Marcin3234 <160618998+Marcin3234@users.noreply.github.com>
2024-03-10 14:20:45 +0100

    Delete zad8.py

--- zad8.py
+++
@@ -1,7 +0,0 @@
-kwadraty = [x**2 for x in range(1, 11)]
-
-lista_slow = ["jabłko", "gruszka", "banan", "truskawka", "malina"]
-dlugosci_slow = [len(slowo) for slowo in lista_slow]
-
-print("Pierwsze 10 liczb kwadratowych:", kwadraty)
-print("Długości słów:", dlugosci_slow)
